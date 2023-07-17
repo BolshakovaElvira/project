4d6f8c593ad62719ebde40b743ddd41386399e7f
elya <elyabolshakova18085@gmail.com>
2023-07-17 20:44:42 +1000

    refact

--- src/main/java/projectThread/Runner.java
+++ src/main/java/projectThread/Runner.java
@@ -8,9 +8,11 @@ public class Runner {
     private static float[] arr = new float[size];
 
     public static void main(String[] args) {
+
         Arrays.fill(arr, 1);
         method1(arr);
         method2(arr);
+        
     }
 
     private static void method1(float[] arr) {
@@ -23,27 +25,36 @@ public class Runner {
         System.out.println(System.currentTimeMillis() - a);
     }
 
-    private static void method2(float[] arr) {
+    public static void method2(float[] arr) {
         float[] arr1 = new float[h];
         float[] arr2 = new float[h];
-
-        long a = System.currentTimeMillis();
-
         System.arraycopy(arr, 0, arr1, 0, h);
         System.arraycopy(arr, h, arr2, 0, h);
+        long a = System.currentTimeMillis();
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < arr1.length; i++) {
+                    arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        };
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < arr2.length; i++) {
+                    arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        };
 
-        MyThread t1 = new MyThread(arr1);
-        MyThread t2 = new MyThread(arr2);
-
-        t1.start();
-        t2.start();
-
-        arr1 = t1.get();
-        arr2 = t2.get();
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+        thread1.start();
+        thread2.start();
 
         System.arraycopy(arr1, 0, arr, 0, h);
         System.arraycopy(arr2, 0, arr, arr1.length, arr2.length);
 
-        System.out.println(System.currentTimeMillis() - a);
     }
 }
