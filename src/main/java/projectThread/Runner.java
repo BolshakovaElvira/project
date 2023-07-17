7e8fb263029b93457e4f42a8736ceae14f74983c
elya <elyabolshakova18085@gmail.com>
2023-07-17 21:12:04 +1000

    refact

--- src/main/java/projectThread/Runner.java
+++ src/main/java/projectThread/Runner.java
@@ -8,8 +8,7 @@ public class Runner {
     private static float[] arr = new float[size];
 
     public static void main(String[] args) {
-
-        Arrays.fill(arr, 1);
+        
         method1(arr);
         method2(arr);
 
@@ -17,7 +16,7 @@ public class Runner {
 
     private static void method1(float[] arr) {
         long a = System.currentTimeMillis();
-
+        Arrays.fill(arr, 1);
         for (int i = 0; i < size; i++) {
             arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
@@ -26,25 +25,29 @@ public class Runner {
     }
 
     public static void method2(float[] arr) {
-        float[] arr1 = new float[h];
-        float[] arr2 = new float[h];
-        System.arraycopy(arr, 0, arr1, 0, h);
-        System.arraycopy(arr, h, arr2, 0, h);
+
         long a = System.currentTimeMillis();
+        Arrays.fill(arr, 1);
         Runnable runnable1 = new Runnable() {
             @Override
             synchronized public void run() {
+                float[] arr1 = new float[h];
+                System.arraycopy(arr, 0, arr1, 0, h);
                 for (int i = 0; i < arr1.length; i++) {
                     arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
+                System.arraycopy(arr1, 0, arr, 0, h);
             }
         };
         Runnable runnable2 = new Runnable() {
             @Override
             synchronized public void run() {
+                float[] arr2 = new float[h];
+                System.arraycopy(arr, h, arr2, 0, h);
                 for (int i = 0; i < arr2.length; i++) {
                     arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
+                System.arraycopy(arr2, 0, arr, h, h);
             }
         };
 
@@ -53,8 +56,10 @@ public class Runner {
         thread1.start();
         thread2.start();
 
-        System.arraycopy(arr1, 0, arr, 0, h);
-        System.arraycopy(arr2, 0, arr, arr1.length, arr2.length);
+
+
+        System.out.println(System.currentTimeMillis()-a);
+
 
     }
 }
